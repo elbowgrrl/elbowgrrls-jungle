0cfda97c92c8afa14b99bef4df200406637f4a83
Megan V <elbowgrrl@gmail.com>
2021-07-22 12:00:55 -0700

    adding dashboard count feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
   require 'dotenv/load'
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+  
   def show
+    @count_products = Product.select("created_at").count
+    @count_categories = Category.select("created_at").count
   end
 end
