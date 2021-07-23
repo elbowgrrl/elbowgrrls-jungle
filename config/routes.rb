d2536b93e6c87bc04342b07390b4f3baa0404f1e
Megan V <elbowgrrl@gmail.com>
2021-07-22 17:00:27 -0700

    adding controller, view and routes for feature

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, :categories, except: [:edit, :update, :show]
   end
 
   get 'about' => 'about#index'
