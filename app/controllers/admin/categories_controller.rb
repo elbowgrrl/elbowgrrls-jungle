789d9de0c194e74198e62d6ae28902de7163ee38
Megan V <elbowgrrl@gmail.com>
2021-07-22 18:14:19 -0700

    adding remaining functionality for adding product categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,13 +1,29 @@
 class Admin::CategoriesController < ApplicationController
+  require 'dotenv/load'
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
 
   def index
-    @admin_category = Category.order(id: :desc).all
+    @category = Category.all
   end
 
-def create
-end
+  def new
+    @category = Category.new
+  end  
 
-def new
-end  
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
 
 end
